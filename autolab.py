07e84eaeba59e9bb09594448e8da336574f6111c
JustinKozlowski <jjkozlow@buffalo.edu>
2022-11-04 22:50:05 -0400

    init

--- autolab.py
+++ autolab.py
@@ -51,7 +51,7 @@ if __name__ == "__main__":
     parser.add_argument('commit')
     args = parser.parse_args()
     if ('UBIT_USERNAME' not in os.environ) or ('UBIT_PASSWORD' not in os.environ) or not os.environ['UBIT_USERNAME'] or os.environ['UBIT_PASSWORD']:
-        print('''Propper secrets/environment variables are not set.
+        Exception('''Propper secrets/environment variables are not set.
 You must have UBIT_USERNAME and UBIT_PASSWORD set as environment variables for this action.
 
 ex:
@@ -62,6 +62,6 @@ ex:
 ''')
     info = parse_commit(args.commit)
     if not info:
-        print('Not a submission Commit, start commit message with "submit " to submit')
+        Exception('Not a submission Commit, start commit message with "submit " to submit: \nAdd if: startsWith(github.event.head_commit.message, "submit ") to this step in the action.yml')
     else:
         main(info)
